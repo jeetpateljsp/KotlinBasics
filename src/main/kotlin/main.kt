2d39f762c708e0281e4030d180ed23d74061fcb9
Jeet Patel <jeetpatel@Jeets-MacBook-Pro.local>
2021-01-18 16:07:01 +0530

    Initial commit. 1. Basic Datatypes and String Interpolation

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,23 @@
+fun main(arguments: Array<String>) {
+    var mMyAge: Int = 23
+    val mFirstName: String = "Jeet"
+    val mLastName = "Patel"
+    val mWeight = 63.5
+
+    /**
+     * calls [main]
+     * variables [arguments].
+     *
+     * @param arguments
+     * */
+
+    /*
+    * */
+
+    //
+
+    println(mFirstName.length)
+
+    val mMessage = "$mFirstName $mLastName is $mMyAge years old, and weighs $mWeight kg."
+    print(mMessage)
+}
