200daf669e608b110a7186a46da5f254a50c75d4
jeetpateljsp <jeetpateljsp@gmail.com>
2021-01-18 16:35:59 +0530

    mSecondTutorial Added!🚀

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,25 @@
 fun main(arguments: Array<String>) {
+
     var mMyAge: Int = 23
     val mFirstName: String = "Jeet"
     val mLastName = "Patel"
     val mWeight = 63.5
+
+    mFirstTutorial(mMyAge, mFirstName, mLastName, mWeight)
+    mSecondTutorial(mMyAge)
+
+}
+
+fun mSecondTutorial(mMyAge: Int) {
+    val areYouAwesome = true
+
+    println("Am i awesome? $areYouAwesome")
+
+    val isLegalDrivingAge = mMyAge >= 18
+    println(isLegalDrivingAge)
+}
+
+fun mFirstTutorial(mMyAge: Int, mFirstName: String, mLastName: String, mWeight: Double) {
 
     /**
      * calls [main]
@@ -19,5 +36,5 @@ fun main(arguments: Array<String>) {
     println(mFirstName.length)
 
     val mMessage = "$mFirstName $mLastName is $mMyAge years old, and weighs $mWeight kg."
-    print(mMessage)
+    println(mMessage)
 }
